a637c333360c02629c09fa22f438dab9d04d840c
Georgi Dimitrov <ggdimitrov@duck.com>
2024-06-24 22:03:16 +0300

    Update ThreatInspector.py

--- ThreatInspector.py
+++ ThreatInspector.py
@@ -6,7 +6,7 @@ def query_abuseipdb(api_key, ip):
     # Mocked function to query AbuseIPDB
     url = f'https://api.abuseipdb.com/api/v2/check?ipAddress={ip}&maxAgeInDays=90'
     headers = {
-        'Key': 'e4b32da34ddd7e8b84cdf76ee42f853696a92b4815742678e756100a092ba0d7a310e6b9a4d9907b',
+        'Key': 'YOUR_API_KEY',# Replace with your actual API key
         'Accept': 'application/json'
     }
     response = requests.get(url, headers=headers)
