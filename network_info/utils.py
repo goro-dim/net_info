813ad7162e7a43559ec1388170fc9fb8946a888e
Georgi Dimitrov <ggdimitrov@duck.com>
2024-06-24 21:44:43 +0300

    Delete network_info directory

--- network_info/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def extract_ips(connections):
-    ip_pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+)')
-    ips = set()
-    for line in connections:
-        matches = ip_pattern.findall(line)
-        ips.update(matches)
-    return list(ips)
